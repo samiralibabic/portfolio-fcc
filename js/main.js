e6ff59c7169657c5986d4aa32152a18f2dfbec06
samiralibabic <samir.alibabic@gmail.com>
2016-04-22 15:12:53 +0200

    Added media queries and corrected display on various device sizes. New (extended) icon set for social icons. Rearranged the menu. Added some CSS and jQuery animations for scrolling.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-    $(".navbar, navbar-default").css("background-color", "!important");
+    /* Navigation smooth scroll */
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top-30
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    /* Change menu background on scrolling */
+    $(document).scroll(function() {
+        if ($("body").scrollTop() > 100) {
+            $("nav").addClass("navbar-scrolled");
+        } else {
+            $("nav").removeClass("navbar-scrolled");
+        }
+    });
 });
